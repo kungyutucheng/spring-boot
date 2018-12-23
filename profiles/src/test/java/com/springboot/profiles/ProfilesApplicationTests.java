4e1f66d50d73a5e368a1ff0854d89c942f5cef50
kungyu@fenghuangjinfu.com <kungyu@fenghuangjinfu.com>
2018-12-23 22:20:43 +0800

    多环境配置

---
+++ profiles/src/test/java/com/springboot/profiles/ProfilesApplicationTests.java
@@ -0,0 +1,22 @@
+package com.springboot.profiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfilesApplicationTests {
+
+	@Value("${env}")
+	private String env;
+
+	@Test
+	public void contextLoads() {
+		System.out.println(env);
+	}
+
+}
+
